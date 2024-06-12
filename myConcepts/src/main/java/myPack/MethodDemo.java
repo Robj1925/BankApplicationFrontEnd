e69c5ff0b9d242c9197947c982f6a6c387c991cf
robj1925 <robj1925@gmail.com>
2024-06-12 12:45:45 -0400

    updated MethodDemo

--- myConcepts/src/main/java/myPack/MethodDemo.java
+++ myConcepts/src/main/java/myPack/MethodDemo.java
@@ -3,8 +3,15 @@ package myPack;
 public class MethodDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] messages = {"HEllo", "My", "Name", "Is: "};
+		displayMessage(messages);
 
+	}
+
+	public static void displayMessage(String... strings) {
+		for (String s: strings) {
+			System.out.println(s);
+		}
 	}
 
 }
