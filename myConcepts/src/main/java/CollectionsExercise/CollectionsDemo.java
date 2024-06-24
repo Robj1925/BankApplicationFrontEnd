7551f226485011e40481fbdbb8ddde4485fb65a0
robj1925 <robj1925@gmail.com>
2024-06-24 17:46:55 -0400

    finished collections exercise

--- myConcepts/src/main/java/CollectionsExercise/CollectionsDemo.java
+++ myConcepts/src/main/java/CollectionsExercise/CollectionsDemo.java
@@ -1,7 +1,12 @@
 package CollectionsExercise;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
 import java.awt.color.*;
@@ -15,6 +20,16 @@ public class CollectionsDemo {
 		
 		String word = "cook";
 		System.out.println(occurencesOfLettersInString(word));
+		System.out.println(numOfDupWordsInASentence("The the THE dog is brown BrOwN"));
+		
+		LinkedList<Integer> example = new LinkedList<>();
+		example.add(1);
+		example.add(2);
+		example.add(3);
+		example.add(4);
+		System.out.println(reverseLinkedlist(example));
+
+		
 	}
 	public static Set<String> noFirstNameDups(String names) {
 		String[] namesArr = names.split(" ");
@@ -31,6 +46,29 @@ public class CollectionsDemo {
 			hm.put(c, hm.getOrDefault(c, 0) + 1);
 		}
 		return hm;
+	}
+	public static int numOfDupWordsInASentence(String sentence) {
+		HashMap<String, Integer> hm = new HashMap<>();
+		Set<String> duplicateWordCount = new TreeSet<>();
+		String[] words = sentence.split(" ");
+		for (String word : words) {
+			word = word.toLowerCase();
+			hm.put(word, hm.getOrDefault(word, 0) + 1);
+
+		}
+		 for (Map.Entry<String, Integer> set :
+             hm.entrySet()) {
+			 if (set.getValue() >= 2) {
+					duplicateWordCount.add(set.getKey());
+			 }
+		 }
+		return duplicateWordCount.size();
+
+	}
+	public static LinkedList<Integer> reverseLinkedlist(LinkedList<Integer> list) {
+		Collections.reverse(list);
+		return list;
+		
 	}
 
 }
