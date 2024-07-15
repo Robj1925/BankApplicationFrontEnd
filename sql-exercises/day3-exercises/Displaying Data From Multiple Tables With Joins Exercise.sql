a2a1d5fdd35ecf2b7086bffaabd618f7190a8c7b
robj1925 <robj1925@gmail.com>
2024-07-15 12:08:37 -0400

    created spring demo project

---
+++ sql-exercises/day3-exercises/Displaying Data From Multiple Tables With Joins Exercise.sql
@@ -0,0 +1,72 @@
+/*Look through Lesson 9b Displaying Data From Multiple Tables with Joins.sql and 
+  run the code examples there, then practice Joins with the excercises below:
+*/
+-- Practice Joining Tables
+
+/* 1) Produce a query that shows addresses of all the departments. Show
+      department name, location id, street address, city, and state / province. 
+      Use NATURAL JOIN to produce your output.*/
+
+
+
+/* 2) Produce a report showing employees who work in Toronto. Display first and 
+      last name, job, department number, department name, and city. 
+      Produce your results with JOIN USING.*/
+     
+
+
+/* 3) Display employee number, last name, manager number, and last name.
+      Label the columns Emp#, Employee, Mgr#, Manager. Show all employees, even
+      if they have no manager. Sort the result by employee id. */
+
+    
+
+
+/* 4) For every department name, show the name of the country where the department
+      is located. Show all countries even if the have no departments. */ 
+
+        
+      
+/* 5) Create a report showing employee first and last name, department number, 
+      and first and last names of their department colleagues. Do not display 
+      the same employee twice in any row. Label the columns appropriately to 
+      clarify the results. Order the results by employee id, even
+      though it is not displayed.*/
+      
+
+      
+      
+/* 6) Display last name, job id, department name, salary, and salary grade level
+      for all employees, even if the employee is not assigned to a department.
+      Order the result by department name, job, and last name.*/ 
+      
+      
+        
+/* 7) Write a query that displays employees who have had different jobs. Show
+      employee number, first and last name, previous job id, previous 
+      job start date and end date. Use appropriate column aliases to clarify 
+      the results. Order the result by employee id and end date. */
+      
+  
+      
+/* 8) Find the first and last names and hire dates of all employees who were 
+      hired before their bosses. Also display the boss's last name 
+      and hire date. Be sure to use appropriate column aliases where needed. */
+      
+    
+        
+  
+/* 9) For every country name, show how many departments are located in that country.
+       Use a column alias to clarify the meaning of the numbers.*/
+       
+
+         
+--------- CUSTOMERS and SALES Tables ----------------------
+
+/* 10)	For every sale, show the id, first and last names, email, and phone of the customer, 
+		the first and last names of the sales representative, and the sales amount. 
+		Order the result by customer id and sales amount.*/
+   
+    
+    
+    
