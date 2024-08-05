ca5242a36629a7db7a8fd49737ce02e48c22b18b
robj1925 <robj1925@gmail.com>
2024-08-05 00:25:58 -0400

    finished implementing find by id, and create an account feature

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrl: './create-account.component.css'
+  styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent {
   accountType: string = 'savings';
+  http: HttpClient = inject(HttpClient);
+  router: Router = inject(Router);
 
   onAccountTypeChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     this.accountType = selectElement.value;
   }
 
-  onSubmit() {
-    console.log('Form submitted');
+  onCreateCustomer(account: { balance: number; accountType: string; customerId: number; interestRate?: number; nextCheckNumber?: number }) {
+    this.http.post('http://localhost:8084/accounts', account).subscribe((res) => {
+      console.table(res);
+      alert('Account created successfully!');
+      this.router.navigate(['/home']);
+    });
   }
-
 }
